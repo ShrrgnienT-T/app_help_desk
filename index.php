f7a0538c5f92b22822a8eb57ffd5952df84a56fc
Eduardo Lima Oliveira <eduardooliveira@ses.int>
2024-05-10 15:48:41 -0400

    include require

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -31,9 +33,8 @@
             <div class="card-header">
               Login
             </div>
-
             <div class="card-body">
-              <form action="http://localhost/app_help_desk/valida_login.php" method="post">
+              <form action="valida_login.php" method="post">
                 <div class="form-group">
                   <input name="email" type="email" class="form-control" placeholder="E-mail">
                 </div>
@@ -41,17 +42,23 @@
                   <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
 
-                <?php
-                  if(isset($_GET['login']) && $_GET['login']== 'erro'){
-                ?>
-                  <div class="text-danger">usuário ou senha inválido</div>
-                <?php
-                  }
-                ?>
+                <?php if(isset($_GET['login']) && $_GET['login'] == 'erro'){?>
+
+                <div class="text-danger">
+                  Usuário ou senha inválido(s)
+                </div>
+
+                <?php } ?>
+                <?php if(isset($_GET['login']) && $_GET['login'] == 'erro2'){?>
+
+                <div class="text-danger">
+                  Por favor, faça login antes de acessar as páginas protegidas
+                </div>
+
+                <?php } ?>
 
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
-
             </div>
           </div>
         </div>
